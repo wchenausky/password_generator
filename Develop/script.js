06ff25e3688f6e383f5660197ce2d3651755d73f
William Cheausky <williamchenausky@yahoo.com>
2020-12-28 16:36:36 -0500

    created global variables and started prompt function with conditional statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,115 @@
 // Assignment Code
+
+//global variables
+
+var lowerCaseLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var upperCaseLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var numberValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  ",",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+//function to get user input using prompts
+function getPasswordCriteria() {
+  var length = parseInt(prompt("How long would you like your password to be?"));
+  //conditional to check if password is at least 8 characters
+  if (length < 8) {
+    alert("Your password has to be at least 8 characters.");
+    return;
+  }
+  //conditional to check if passwrod is less than 128 characters
+  if (length > 128) {
+    alert("Your password must be less than 128 characters.");
+    return;
+  }
+}
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
-}
-var passwordLength = prompt("How Long Would You Like Your Password?");
-if (passwordLength < 8) {
-alert("Choose A Number Greater Than 8 Character");
-}
-if (passwordLength > 128) {
-alert("Choose A Number Less Than 128 Characters");
-}
-
-function generatePassword() {
-  var length = 8,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-      retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
-  }
-  return retVal;
 }
 
 // Add event listener to generate button
