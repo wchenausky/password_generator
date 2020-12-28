a24768b969b113aa1c1e083b343c2762d8363e0f
William Cheausky <williamchenausky@yahoo.com>
2020-12-28 12:22:17 -0500

    added password character alert

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,23 @@ function writePassword() {
   passwordText.value = password;
 
 }
+var passwordLength = prompt("How Long Would You Like Your Password?");
+if (passwordLength < 8) {
+alert("Choose A Number Greater Than 8 Character");
+}
+if (passwordLength > 128) {
+alert("Choose A Number Less Than 128 Characters");
+}
+
+function generatePassword() {
+  var length = 8,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
