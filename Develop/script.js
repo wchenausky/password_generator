f1b8c2dd082ba909eded3784908b0ac9ada4a0f5
William Cheausky <williamchenausky@yahoo.com>
2020-12-28 17:00:07 -0500

    added variables and reutrn for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -91,16 +91,49 @@ var specialCharacters = [
 //function to get user input using prompts
 function getPasswordCriteria() {
   var length = parseInt(prompt("How long would you like your password to be?"));
-  //conditional to check if password is at least 8 characters
+
+  //conditional statement to validate if input is a number or not. prompt ends if false.
+  if (isNaN(length) === true) {
+    alert("You need to ick a number please.");
+    return;
+  }
+  //conditional to check if password is at least 8 characters. prompt ends if false.
   if (length < 8) {
     alert("Your password has to be at least 8 characters.");
     return;
   }
-  //conditional to check if passwrod is less than 128 characters
+  //conditional to check if passwrod is less than 128 characters. prompt ends if false
   if (length > 128) {
     alert("Your password must be less than 128 characters.");
     return;
   }
+// Variableto store boolean if user wants to lower case letters
+  var wantLowerCaseLetters = confrim("Click OK if you want lower case letters");
+  // Variable to store boolean if user wants upper case letters
+  var wantUpperCaseLetters = confrim("Click OK if you want upper case letters");
+  // Variable to store boolean if user wants numbers
+  var numberValues = confrim("Click OK if you want numbers");
+  //Variable to store boolean if user wants special characters
+  var specialCharacters = confrim("Click OK if you want special characters");
+
+  if (
+wantLowerCaseLetters === false &&
+wantUpperCaseLetters=== false &&
+numberValues === false &&
+specialCharacters === false 
+) {
+  alert("you have to select at least one character type")
+}
+
+var passwordCriteria ={
+  length: length,
+  wantLowerCaseLetters: wantLowerCaseLetters,
+  wantUpperCaseLetters: wantUpperCaseLetters,
+  numberValues: numberValues,
+  specialCharacters: specialCharacters,
+};
+
+return passwordCriteria;
 }
 
 var generateBtn = document.querySelector("#generate");
