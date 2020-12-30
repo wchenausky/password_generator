f6c5005a60873e209b7109fb3a9c2b8191bd136f
William Cheausky <williamchenausky@yahoo.com>
2020-12-29 19:06:24 -0500

    update password options

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,8 @@
 
 //global variables
 
+//function to get user input using prompts
+
 var lowerCaseLetters = [
   "a",
   "b",
@@ -30,7 +32,6 @@ var lowerCaseLetters = [
   "y",
   "z",
 ];
-
 var upperCaseLetters = [
   "A",
   "B",
@@ -88,13 +89,13 @@ var specialCharacters = [
   "_",
   ".",
 ];
-//function to get user input using prompts
+
 function getPasswordCriteria() {
   var length = parseInt(prompt("How long would you like your password to be?"));
 
   //conditional statement to validate if input is a number or not. prompt ends if false.
   if (isNaN(length) === true) {
-    alert("You need to ick a number please.");
+    alert("You need to pick a number please.");
     return;
   }
   //conditional to check if password is at least 8 characters. prompt ends if false.
@@ -103,47 +104,87 @@ function getPasswordCriteria() {
     return;
   }
   //conditional to check if passwrod is less than 128 characters. prompt ends if false
-  if (length > 128) {
+  if (length > 129) {
     alert("Your password must be less than 128 characters.");
     return;
   }
-// Variableto store boolean if user wants to lower case letters
+// Variable to store boolean if user wants to lower case letters
   var wantLowerCaseLetters = confrim("Click OK if you want lower case letters");
   // Variable to store boolean if user wants upper case letters
   var wantUpperCaseLetters = confrim("Click OK if you want upper case letters");
   // Variable to store boolean if user wants numbers
-  var numberValues = confrim("Click OK if you want numbers");
+  var wantNumberValues = confrim("Click OK if you want numbers");
   //Variable to store boolean if user wants special characters
-  var specialCharacters = confrim("Click OK if you want special characters");
+  var wantSpecialCharacters = confrim("Click OK if you want special characters");
 
   if (
 wantLowerCaseLetters === false &&
 wantUpperCaseLetters=== false &&
-numberValues === false &&
-specialCharacters === false 
+wantNumberValues === false &&
+wantSpecialCharacters === false 
 ) {
-  alert("you have to select at least one character type")
+  alert("you have to select at least one character type");
+  return;
 }
 
 var passwordCriteria ={
   length: length,
   wantLowerCaseLetters: wantLowerCaseLetters,
   wantUpperCaseLetters: wantUpperCaseLetters,
-  numberValues: numberValues,
-  specialCharacters: specialCharacters,
+  wantNumberValues: wantNumberValues,
+  wantSpecialCharacters: wantSpecialCharacters,
 };
 
 return passwordCriteria;
 }
 
+function createPassword() {
+  var options = getPasswordCriteria();
+  console.log(options)
+
+  var passwordEntry = [];
+console.log(passwordEntry)
+
+if (options.lowerCaseLetters) {
+  for (i =0 ; i < wantLowerCaseLetters.length; ++i) {
+    passwordEntry.push(wantLowerCaseLetters[i]);
+  }
+}
+if (options.specialCharacters) {
+  for (i = 0; i < wantSpecialCharacters.length; ++i) {
+      passwordEntry.push(wantSpecialCharacters[i]);
+  }
+} 
+if (options.numberValues) {
+  for (i = 0; i < wantNumberValues.length; ++i) {
+  passwordEntry.push(wantNumberValues[i]);
+  }
+}
+if (options.upperCaseLetters) {
+  for (i = 0; i < wantUpperCaseLetters.length; ++i) {
+  passwordEntry.push(wantUpperCaseLetters[i]);
+  }
+}
+
+var password = [];
+
+for (let i = 0; i < options.length; ++i) {
+  var randomPicker = Math.floor(Math.random() * Math.floor(passwordEntry.length));
+   password.push(passwordEntry[randomPicker])
+}
+
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
